ed4eaae3e4733c4239d201462088124952b30f4d
TERRYYYY <kariuki.terry11@gmail.com>
2023-07-18 16:41:23 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+/* Scrolling circular text */
+
+// Function for scrolling circular text
+const text = document.getElementById('text');
+
+// Variable to make the text circular
+const rotate = new CircleType(text).radius(50);
+
+//Event to rotate when scrolling up and down
+window.addEventListener('scroll', function(){
+text.style.transform = 'rotate(' + (window.scrollY * 0.15) + 'deg)'
+});
+
+
+/* Change Background Color */
+// Function to change background color when scrolling
+function changeBackground(){
+    if(window.scrollY > window.innerHeight / 2){
+            document.body.classList.add('bg-color');
+    }else{
+            document.body.classList.remove('bg-color');
+        }
+};
+
+window.addEventListener('scroll', changeBackground);
